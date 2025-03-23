4651ba113b9f2f1c2d3904b2e1c3c9dc8657f98c
Andréa <andrea.marnier@gmail.com>
2025-03-23 21:44:10 +0100

    refactor(split address): user edit page

--- kdance/members/static/js/user_edit.js
+++ kdance/members/static/js/user_edit.js
@@ -13,6 +13,8 @@ function getUser() {
       $('#edit-me-email').val(data.email);
       $('#edit-me-phone').val(data.profile.phone);
       $('#edit-me-address').val(data.profile.address);
+      $('#edit-me-postal-code').val(data.profile.postal_code);
+      $('#edit-me-city').val(data.profile.city);
     },
     error: (error) => {
       showToast('Impossible de récupérer vos informations pour le moment.');
@@ -33,6 +35,8 @@ function patchUser() {
       profile: {
         phone: $('#edit-me-phone').val(),
         address: $('#edit-me-address').val(),
+        postal_code: $('#edit-me-postal-code').val(),
+        city: $('#edit-me-city').val(),
       }
     };
     $.ajax({
@@ -59,6 +63,18 @@ function patchUser() {
         if (error.responseJSON && error.responseJSON.email) {
           $('#invalid-edit-me-email').html(error.responseJSON.email[0]);
           $('#invalid-edit-me-email').addClass('d-inline');
+        }
+        if (error.responseJSON && error.responseJSON.address) {
+          $('#invalid-edit-me-address').html(error.responseJSON.address[0]);
+          $('#invalid-edit-me-address').addClass('d-inline');
+        }
+        if (error.responseJSON && error.responseJSON.postal_code) {
+          $('#invalid-edit-me-postal-code').html(error.responseJSON.postal_code[0]);
+          $('#invalid-edit-me-postal-code').addClass('d-inline');
+        }
+        if (error.responseJSON && error.responseJSON.city) {
+          $('#invalid-edit-me-city').html(error.responseJSON.city[0]);
+          $('#invalid-edit-me-city').addClass('d-inline');
         }
       }
     });
