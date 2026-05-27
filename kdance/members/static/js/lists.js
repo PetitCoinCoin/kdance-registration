c8f88ffbd935b92aae4ef7377a4bf72d2a3eea2e
Andréa <andrea.marnier@gmail.com>
2026-05-27 22:17:32 +0200

    fix: preview des cours de l'an prochain

--- kdance/members/static/js/lists.js
+++ kdance/members/static/js/lists.js
@@ -61,6 +61,9 @@ function populateSecondSelect(previousValue) {
     case '8':
       getCourses($('#season-select').val(), previousValue);
       break
+    case '9':
+      getCourses(nextSeasonId, previousValue);
+      break
     case '3':
       $('#menu-2-select').empty();
       for (let [key, value] of Object.entries(MONTH)) {
@@ -88,7 +91,7 @@ function getCourses(seasonId, mainValue) {
     url: coursesUrl + `?season=${seasonId}`,
     type: 'GET',
     success: (data) => {
-      $('#menu-2-select').append($('<option>', { value: '0', text: ['6', '8'].indexOf(mainValue) > -1 ? '-' : 'Tous les cours', selected: true }));
+      $('#menu-2-select').append($('<option>', { value: '0', text: ['6', '8', '9'].indexOf(mainValue) > -1 ? '-' : 'Tous les cours', selected: true }));
       for (let i = 0; i < data.length; i++) {
         const startHour = data[i].start_hour.split(':');
         const label = `${data[i].name}, ${WEEKDAY[data[i].weekday]} ${startHour[0]}h${startHour[1]}`;
@@ -113,6 +116,7 @@ function searchData() {
       case '6':
       case '7':
       case '8':
+      case '9':
         getMembersPerCourse(mainValue);
         break
       case '2':
@@ -131,9 +135,9 @@ function getMembersPerCourse(mainValue) {
   document.querySelector('#total-amount-div').className = 'd-none';
   var url;
   const subValue = $('#menu-2-select').val()
+  let filter = '';
   switch (subValue) {
     case null:
-      let filter = '';
       switch (mainValue) {
         case '4':
           filter = 'with_pass';
@@ -148,7 +152,7 @@ function getMembersPerCourse(mainValue) {
       url = `${membersUrl}?season=${$('#season-select').val()}&${filter}=true`;
       break
     case '0':
-      if (mainValue === '6' || mainValue === '8') {
+      if (mainValue === '6' || mainValue === '8' || mainValue === '9') {
         const toast = bootstrap.Toast.getOrCreateInstance(document.getElementById('list-error-toast'));
         $('#list-error-body').text('Veuillez sélectionner un cours.');
         toast.show();
@@ -157,7 +161,10 @@ function getMembersPerCourse(mainValue) {
       url = `${membersUrl}?season=${$('#season-select').val()}`;
       break
     default:
-      url = `${membersUrl}?course=${subValue}`;
+      if (mainValue == '9') {
+        filter = '&next=true'
+      }
+      url = `${membersUrl}?course=${subValue}${filter}`;
   }
   $.ajax({
     url: url,
@@ -166,7 +173,8 @@ function getMembersPerCourse(mainValue) {
       $('#data-table').bootstrapTable('destroy');
       switch (mainValue) {
         case '1':
-          buildMembersInfo(data, subValue);
+        case '9':
+          buildMembersInfo(data, subValue, mainValue === '9');
           break
         case '2':
         case '3':
@@ -202,7 +210,7 @@ function getMembersPerCourse(mainValue) {
   });
 }
 
-function buildMembersInfo(data, courseId) {
+function buildMembersInfo(data, courseId, isForNextSeason) {
   let columns = [
     {
       field: 'name',
@@ -210,6 +218,26 @@ function buildMembersInfo(data, courseId) {
       searchable: true,
       sortable: true,
     },
+    {
+      field: 'birthday',
+      title: 'Date de naissance',
+      searchable: false,
+      sortable: true,
+    }
+  ];
+  if (isForNextSeason) {
+    columns.push({
+        field: 'current_courses',
+        title: 'Cours actuels',
+        searchable: false,
+        sortable: true,
+        visible: true,
+        formatter: function(value) {
+          return value.join('<br />')
+        },
+      })
+  } else {
+    columns.push(...[
     {
       field: 'created',
       title: 'Inscrit le',
@@ -225,11 +253,6 @@ function buildMembersInfo(data, courseId) {
       formatter: function(value) {
         return value.join('<br />')
       },
-    }, {
-      field: 'birthday',
-      title: 'Date de naissance',
-      searchable: false,
-      sortable: true,
     }, {
       field: 'phone',
       title: 'Téléphone',
@@ -330,19 +353,20 @@ function buildMembersInfo(data, courseId) {
       sortable: true,
       visible: false,
     }
-  ];
-  if (courseId > 0) {
-    columns.splice(5, 0, {
-      field: 'courses',
-      title: 'Autre cours',
-      searchable: true,
-      sortable: true,
-      visible: true,
-      formatter: function(value) {
-        return value.join('<br />')
-      },
+  ]);
+    if (courseId > 0) {
+      columns.splice(5, 0, {
+        field: 'courses',
+        title: 'Autre cours',
+        searchable: true,
+        sortable: true,
+        visible: true,
+        formatter: function(value) {
+          return value.join('<br />')
+        },
+      }
+    );
     }
-  );
   }
   $('#data-table').bootstrapTable({
     ...COMMON_TABLE_PARAMS,
@@ -356,6 +380,17 @@ function buildMembersInfo(data, courseId) {
     },
     columns: columns,
     data: data.map(m => {
+      if (isForNextSeason) {
+        return {
+          birthday: (new Date(m.birthday)).toLocaleDateString('fr-FR'),
+          name: `${m.last_name} ${m.first_name}`,
+          current_courses: m.active_courses.map(
+          (c) => `${c.name}, ${WEEKDAY[c.weekday]}`
+        ).concat(m.cancelled_courses.map(
+          (c) => `${c.name}, ${WEEKDAY[c.weekday]} (Annulé)`)
+        ),
+        }
+      }
       return {
         ...m,
         status: courseId > 0 ? buildStatusOneCourse(m, courseId) : buildStatusAllCourses(m),
