a7fe6cee24ffc29b25e31323bbf91f38e63c0511
Andréa <andrea.marnier@gmail.com>
2026-05-31 11:51:32 +0200

    fix: update settings

--- kdance/kdance/settings.py
+++ kdance/kdance/settings.py
@@ -159,11 +159,11 @@ SUPERUSER_PWD = os.getenv("SUPERUSER_PWD")
 LOGIN_URL = "/login/"
 LOGOUT_REDIRECT_URL = "/login/"
 CSRF_COOKIE_HTTPONLY = True
-SESSION_COOKIE_AGE = 60 * 60 * 24 * 7  # 1 week
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 60  # 60 days
 
 # Internationalization
 LANGUAGE_CODE = "fr-fr"
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Paris"
 USE_I18N = True
 USE_TZ = True
 
