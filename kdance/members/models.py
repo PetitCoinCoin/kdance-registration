a442e518615581ed17297cf5e9479cb45896834c
Andréa <andrea.marnier@gmail.com>
2026-08-02 11:36:13 +0200

    fix: (pre)signup dates

--- kdance/members/models.py
+++ kdance/members/models.py
@@ -150,28 +150,23 @@ class Season(models.Model):
     @property
     def is_pre_signup_ongoing(self) -> bool:
         today = date.today()
-        pre_signup_end = self.pre_signup_end + timedelta(
-            days=GeneralSettings.get_solo().pre_signup_payment_delta_days
-        )
-        return self.pre_signup_start <= today <= pre_signup_end
+        return self.pre_signup_start <= today <= self.pre_signup_end
 
     @property
     def is_before_signup(self) -> bool:
-        return (
-            self.signup_start is not None
-            and self.signup_end is not None
-            and date.today() <= self.signup_end
+        if self.signup_end is None:
+            return False
+        signup_end = self.signup_end + timedelta(
+            days=GeneralSettings.get_solo().pre_signup_payment_delta_days
         )
+        return self.signup_start is not None and date.today() <= signup_end
 
     @property
     def is_signup_ongoing(self) -> bool:
         if not self.signup_start or not self.signup_end:
             return False
         today = date.today()
-        signup_end = self.signup_end + timedelta(
-            days=GeneralSettings.get_solo().signup_payment_delta_days
-        )
-        return self.signup_start <= today <= signup_end
+        return self.signup_start <= today <= self.signup_end
 
     @property
     def is_after_signup(self) -> bool:
